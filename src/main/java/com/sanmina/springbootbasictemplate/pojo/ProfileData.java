0fd113251679333a98d45a4f65da79a7ad63bd45
NestorME <nestoresp.22.10@gmail.com>
2018-12-03 11:49:33 -0600

    Initial commit

---
+++ src/main/java/com/sanmina/springbootbasictemplate/pojo/ProfileData.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sanmina.springbootbasictemplate.pojo;
+
+/**
+ *
+ * @author nestor_milian
+ */
+class ProfileData {
+    private Long idProfile;
+    private String profile;
+
+    public Long getIdProfile() {
+        return idProfile;
+    }
+
+    public void setIdProfile(Long idProfile) {
+        this.idProfile = idProfile;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+    
+    
+}
---
+++ src/main/java/com/sanmina/springbootbasictemplate/pojo/ResponseApi.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sanmina.springbootbasictemplate.pojo;
+
+/**
+ *
+ * @author nestor_milian
+ */
+public class ResponseApi {
+    private Boolean success;
+    private String message;
+    private String title;
+    private Integer code;
+    private Object data;
+    private Object data2;
+    private Object data3;
+    private String type;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Object getData2() {
+        return data2;
+    }
+
+    public void setData2(Object data2) {
+        this.data2 = data2;
+    }
+
+    public Object getData3() {
+        return data3;
+    }
+
+    public void setData3(Object data3) {
+        this.data3 = data3;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
