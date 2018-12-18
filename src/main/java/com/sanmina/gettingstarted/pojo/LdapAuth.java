b78e4d85fead356013f349ad50f72b74a6638587
Jorge Covarrubias <jorge.covarrubias@sanmina.com>
2018-12-17 18:41:37 -0600

    SpringbootBasicTemplate * Intranet Security Login

---
+++ src/main/java/com/sanmina/gettingstarted/pojo/LdapAuth.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sanmina.gettingstarted.pojo;
+
+/**
+ *
+ * @author nestor_milian
+ */
+public class LdapAuth {
+        private Boolean success;
+    private String message;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
